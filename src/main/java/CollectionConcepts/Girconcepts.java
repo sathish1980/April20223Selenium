e728328af80996dab63a487f524042e12bfbda2d
sathish1980 <kumar.sathish189@gmail.com>
2023-05-28 08:59:44 +0530

    new changes

--- src/main/java/CollectionConcepts/Girconcepts.java
+++ src/main/java/CollectionConcepts/Girconcepts.java
@@ -1,6 +1,11 @@
 package CollectionConcepts;
 
 public class Girconcepts {
+	
+	public void adddata()
+	{
+		System.out.println("New add");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
